ae1bed4e062c055f93486ed3a118c80e6238ee54
Rishu Pandey <80598009+Rishurp@users.noreply.github.com>
2023-06-25 21:08:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,19 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(function(req, res, next) {
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
